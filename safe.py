191011766b3cbbd67674c1057be7657a17f60427
bencouser <73387835+bencouser@users.noreply.github.com>
2021-01-25 12:33:43 +0000

    Add files via upload
    first attempt at making passcode induced servo unlock. No time to test yet. After hopefully a successful test I will then learn how to use a key card sensor to provide the second layer of authentication. I also want to make it so that after showing card you have 3 attempts at the code as the keypad is a little over sensitive. Maybe I should introduce a sleep in order to prevent double clicks.

--- safe.py
+++ safe.py
@@ -14,3 +14,52 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
+
+# Key Code setup
+PASSWORD = [4, 7, 8, 2]
+
+MATRIX = [[1, 2, 3, 'A'],
+          [4, 5, 6, 'B'],
+          [7, 8, 9, 'C'],
+          ['*', 0, '#', 'D']]
+
+ROW = [7, 11, 13, 15]
+COL = [12, 16, 18, 22]
+
+# Servo setup
+GPIO.setup(29, GPIO.OUT)
+
+p = GPIO.PWM(7, 50)
+p.start(7.5)
+
+try:
+    while(True):
+        print("What is the index of the first term in the Fibonacci sequence to contain 1000 digits?")
+        numberPresses = 0
+        guess = []
+        while(numberPresses < 4):
+            for j in range(4):
+                GPIO.output(COL[j], 0)
+
+                for i in range(4):
+                    if GPIO.input(ROW[i]) == 0:
+                        print MATRIX[i][j]
+                        guess.append(MATRIX[i][j])
+                        numberPresses += 1
+                        while(GPIO.input(ROW[i]) == 0):
+                            pass
+
+                GPIO.output(COL[i][j], 1)
+
+        if guess == PASSWORD:
+            print("CORRECT ANSWER")
+            p.ChangeDutyCycle(12.5)
+        else:
+            print("INCORRECT ANSWER!")
+
+        
+
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
